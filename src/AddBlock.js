b45847a473558d036d7861f897e96bdaf495fd75
Rike <hello@ueberdiespree.de>
2019-07-07 11:28:28 +0200

    set tooltip

--- src/AddBlock.js
+++ src/AddBlock.js
@@ -1,21 +1,39 @@
-import React from "react";
-import MyCard from "./MyCard";
-import { Button, Header, Image, Modal } from "semantic-ui-react";
+import React, { useState } from 'react';
+import { Button, Popup, Modal } from "semantic-ui-react";
 
 function AddBlock() {
+
+  const [isTipToolOpen, switchTipTool] = useState(true);
+
+  const button = (
+    <div class="add-button">
+      <button onClick={() => switchTipTool(false)} class="ui circular icon button circle-button">
+        <i aria-hidden="true" class="plus icon" />
+      </button>
+    </div>
+  );
+
   return (
-    <Modal
-      trigger={
-        <div class="add-button">
-          <button class="ui circular icon button circle-button">
-            <i aria-hidden="true" class="plus icon" />
-          </button>
-        </div>
-      }
-      header="Reminder!"
-      content="Call Benjamin regarding the reports."
-      actions={["Snooze", { key: "done", content: "Done", positive: true }]}
-    />
+    <div>
+      <Popup
+        content={
+          <span>
+            Add a <br />
+            new block!
+          </span>
+        }
+        open={isTipToolOpen}
+        position="top right"
+        trigger={button}
+      />
+
+      <Modal
+        trigger={button}
+        header="Reminder!"
+        content="Call Benjamin regarding the reports."
+        actions={["Snooze", { key: "done", content: "Done", positive: true }]}
+      />
+    </div>
   );
 }
 
